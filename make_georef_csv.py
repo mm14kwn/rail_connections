7c3313fb24b465040ac080e70005f4e7a3d0842b
kwn_x220 <newman.kieranw@gmail.com>
2019-01-09 20:05:41 +0000

    geo_csv fixed and run for london, brum and manc

---
+++ make_georef_csv.py
@@ -0,0 +1,54 @@
+import numpy as np
+import csv
+import pickle
+import os
+
+
+def grcsv(destination='london',
+          destpath='/home/kinew/stations/results/',
+          llpath='/home/kinew/stations/station_latlon.csv',
+          outfile='/home/kinew/stations/results/london_geo.csv'):
+    # load in connection data
+    with open(
+            os.path.join(destpath,
+                         '{0}_connections.pickle'.format(destination)),
+            'rb') as f:
+        pdict = pickle.load(f)
+
+    with open(llpath) as f:
+        read_csv = csv.reader(f, delimiter=',')
+        hcount = 0
+        headers = []
+        csvdict = {}
+        for row in read_csv:
+            if hcount == 0:
+                for column in row:
+                    headers.append(column)
+                    csvdict[column] = []
+                hcount = 1
+            else:
+                for count, column in enumerate(row):
+                    csvdict[headers[count]].append(column)
+
+    stllcodes = csvdict['TLC']
+    stlat = csvdict['Latitude']
+    stlon = csvdict['Longitude']
+    pdict['lat'] = []
+    pdict['lon'] = []
+    for code in pdict['codes']:
+        if code in stllcodes:
+            pdict['lat'].append(stlat[stllcodes.index(code)])
+            pdict['lon'].append(stlon[stllcodes.index(code)])
+        else:
+            print('station {0} not in llfile'.format(code))
+            pdict['lat'].append(np.NaN)
+            pdict['lon'].append(np.NaN)
+
+    with open(outfile, 'w', newline='') as f:
+        station_writer = csv.writer(f, delimiter=',')
+        station_writer.writerow(['Name', 'Code', 'Changes', 'lat', 'lon'])
+        for name, code, change, lat, lon in zip(pdict['names'], pdict['codes'],
+                                                pdict['changes'], pdict['lat'],
+                                                pdict['lon']):
+            station_writer.writerow([name, code, change, lat, lon])
+    print('ALL DONE :)')
